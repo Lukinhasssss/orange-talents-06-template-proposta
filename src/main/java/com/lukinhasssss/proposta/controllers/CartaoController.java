b7c915395bf9f7f0722e25cecbc38fb719f6f196
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-07-21 15:23:57 -0300

    Notificando o sistema legado sobre o bloqueio do cartÃao

--- src/main/java/com/lukinhasssss/proposta/controllers/CartaoController.java
+++ src/main/java/com/lukinhasssss/proposta/controllers/CartaoController.java
@@ -9,6 +9,7 @@ import com.lukinhasssss.proposta.repositories.BiometriaRepository;
 import com.lukinhasssss.proposta.repositories.BloqueioCartaoRepository;
 import com.lukinhasssss.proposta.utils.MensagemDeErroNotFound;
 import feign.FeignException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +18,8 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -33,7 +36,10 @@ public class CartaoController {
         this.bloqueioCartaoRepository = bloqueioCartaoRepository;
     }
 
-    @PostMapping("/{id}/biometria")
+    @Value("${sistema.nome}")
+    private String sistemaResponsavel;
+
+    @PostMapping("/{id}/biometrias")
     public ResponseEntity<?> associarBiometria(@PathVariable String id, @RequestBody @Valid BiometriaRequest request) {
         try {
             CartaoResponse cartao = cartaoIntegration.getCartao(id); // Somente valida se o cartão existe
@@ -51,7 +57,7 @@ public class CartaoController {
         return ResponseEntity.created(uri).build();
     }
 
-    @PostMapping("/{id}/bloqueio")
+    @PostMapping("/{id}/bloqueios")
     public ResponseEntity<?> bloquearCartao(@PathVariable String id, @RequestHeader(value = "User-Agent") String userAgent, HttpServletRequest request) {
         Optional<BloqueioCartao> cartaoBloqueado = bloqueioCartaoRepository.findByIdCartao(id);
 
@@ -60,6 +66,10 @@ public class CartaoController {
 
         try {
             CartaoResponse cartao = cartaoIntegration.getCartao(id); // Somente valida se o cartão existe
+
+            Map<String, Object> nomeSistema = new HashMap<>();
+            nomeSistema.put("sistemaResponsavel", this.sistemaResponsavel);
+            cartaoIntegration.bloquearCartaoNoSistemaLegado(id, nomeSistema);
         } catch (FeignException e) {
             if (e.status() == 404)
                 return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new MensagemDeErroNotFound("Não foi possível encontrar um cartão com id: " + id));
@@ -70,8 +80,7 @@ public class CartaoController {
         BloqueioCartao bloqueioCartao = new BloqueioCartao(request.getRemoteAddr(), userAgent, id);
         bloqueioCartaoRepository.save(bloqueioCartao);
 
-        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(bloqueioCartao.getId()).toUri();
-        return ResponseEntity.created(uri).build();
+        return ResponseEntity.ok().build();
     }
 
 }
--- src/main/java/com/lukinhasssss/proposta/integrations/CartaoIntegration.java
+++ src/main/java/com/lukinhasssss/proposta/integrations/CartaoIntegration.java
@@ -4,13 +4,15 @@ import com.lukinhasssss.proposta.dto.response.CartaoResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
-import java.util.Optional;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @FeignClient(url = "${cartao.url}", name = "cartaoIntegration")
 public interface CartaoIntegration {
 
     @GetMapping("/{id}")
     CartaoResponse getCartao(@PathVariable String id);
+
+    @PostMapping("/{id}/bloqueios")
+    void bloquearCartaoNoSistemaLegado(@PathVariable String id, Object sistemaResponsavel);
 
 }
